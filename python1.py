0cf9a369131bc646c9630d6346387046139f338f
LinuxHunt-Nexus <linuxhuntnexus@gmail.com>
2024-10-13 12:57:14 +0600

    second edit python1.py

--- python1.py
+++ python1.py
@@ -1,1 +1,5 @@
-This creates a parent_dir directory with two subdirectories, subdir1 and subdir2. If parent_dir doesn't exist, it will be created automatically.
+This creates a parent_dir directory
+with two subdirectories,
+subdir1 and subdir2.
+If parent_dir doesn't exist,
+it will be created automatically.
